f2d4d7178655f805ad3a27c44d85cb0e52f4dac9
Marek Bendík <mbendik@blueberryapps.com>
2015-03-23 16:39:25 +0100

    Initial Commit

---
+++ ipad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ipad
+//
+//  Created by Marek Bendík on 23/03/15.
+//  Copyright (c) 2015 Marek Bendík. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
